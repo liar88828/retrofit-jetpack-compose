999d9b00d3fbd63e070d146085f7c0a1b539f9a8
brian <liar88828@gmail.com>
2024-08-05 01:53:08 +0700

    init

---
+++ app/src/main/java/com/tutor/retrofit_app/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.tutor.retrofit_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import com.tutor.retrofit_app.ui.theme.Retrofit_appTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			Retrofit_appTheme {
+				Text("Hello")
+			}
+		}
+	}
+}
+
